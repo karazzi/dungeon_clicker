bee28ee9cd0f5ff274b66c5dd6fc53a9023f03d1
Karazzi <fsteenfeldt@gmail.com>
2019-04-21 16:11:17 +0200

    Offline production

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 function GameData() {
-  this.version = "babyenemies";
+  this.version = "babytest4";
   this.gold = null;
-  this.goldPerClick = 1;
+  this.goldPerClick = 100;
   this.gpsTotal = 0;
   this.updateInterval = 10; //milliseconds
+  this.offlineMultiplier = 0.5;
   this.productionMultiplier = 1;
   this.costMultiplier = 1.15;
-  this.lastSave = null;
-  this.lastSaveMS = null;
+  this.lastSave = new Date();
+  this.lastSaveMS = Date.now();
+  this.lastUpdated = Date.now();
   this.quests = {
-    delivery: new Quest("Delivery", 0, 0.1, 15, 1, this.costMultiplier),
-    rats: new Quest("Rats", 0, 1, 100, 1, this.costMultiplier),
-    kobolds: new Quest("Kobolds", 0, 8, 1100, 1, this.costMultiplier),
-    goblins: new Quest("Goblins", 0, 47, 12000, 1, this.costMultiplier),
-    orcs: new Quest("Orcs", 0, 260, 130000, 1, this.costMultiplier),
-    underdark: new Quest("Underdark", 0, 1400, 1400000, 1, this.costMultiplier),
-    beholder: new Quest("Beholder", 0, 7800, 20000000, 1, this.costMultiplier),
-    youngDragon: new Quest("Young Dragon", 0, 44000, 330000000, 1, this.costMultiplier),
-    lich: new Quest("Lich", 0, 260000, 5100000000, 1, this.costMultiplier),
-    ancientDragon: new Quest("Ancient Dragon", 0, 1600000, 75000000000, 1, this.costMultiplier),
-    demigod: new Quest("Demigod", 0, 10000000, 1000000000000, 1, this.costMultiplier)
+    delivery: new Quest("delivery", 0, 0.1, 15, 1, this.costMultiplier),
+    rats: new Quest("rats", 0, 1, 100, 1, this.costMultiplier),
+    kobolds: new Quest("kobolds", 0, 8, 1100, 1, this.costMultiplier),
+    goblins: new Quest("goblins", 0, 47, 12000, 1, this.costMultiplier),
+    orcs: new Quest("orcs", 0, 260, 130000, 1, this.costMultiplier),
+    underdark: new Quest("underdark", 0, 1400, 1400000, 1, this.costMultiplier),
+    beholder: new Quest("beholder", 0, 7800, 20000000, 1, this.costMultiplier),
+    youngDragon: new Quest("youngDragon", 0, 44000, 330000000, 1, this.costMultiplier),
+    lich: new Quest("lich", 0, 260000, 5100000000, 1, this.costMultiplier),
+    ancientDragon: new Quest("ancientDragon", 0, 1600000, 75000000000, 1, this.costMultiplier),
+    demigod: new Quest("demigod", 0, 10000000, 1000000000000, 1, this.costMultiplier)
   }
 }
 
@@ -39,18 +41,23 @@ function initGameData() {
   if (savegame !== null) {
     if (savegame.version == game.version) {
       game = savegame;
+      game.lastUpdated = Date.now(); // Reset timer
     }else{
       if (typeof savegame.gold !== "undefined") game.gold = savegame.gold;
       if (typeof savegame.goldPerClick !== "undefined") game.goldPerClick = savegame.goldPerClick;
-      if (typeof savegame.gpsTotal !== "undefined") game.gpsTotal = savegame.gpsTotal;
+      // if (typeof savegame.gpsTotal !== "undefined") game.gpsTotal = savegame.gpsTotal;
       if (typeof savegame.productionMultiplier !== "undefined") game.productionMultiplier = savegame.productionMultiplier;
-      if (typeof savegame.costMultiplier !== "undefined") game.costMultiplier = savegame.costMultiplier;
+      // if (typeof savegame.costMultiplier !== "undefined") game.costMultiplier = savegame.costMultiplier;
       if (typeof savegame.lastSave !== "undefined") game.lastSave = savegame.lastSave;
       if (typeof savegame.lastSaveMS !== "undefined") game.lastSaveMS = savegame.lastSaveMS;
       for(quest in game.quests) {
         if(typeof savegame.quests[quest] !== "undefined"){
-          for (property in game.quests[quest]){
-            if (typeof savegame.quests[quest][property] !== "undefined") game.quests[quest][property] = savegame.quests[quest][property];
+          // for (property in game.quests[quest]){
+          //   if (typeof savegame.quests[quest][property] !== "undefined") game.quests[quest][property] = savegame.quests[quest][property];
+          // }
+          if (typeof savegame.quests[quest].owned !== "undefined") {
+            game.quests[quest].owned = savegame.quests[quest].owned;
+            game.quests[quest].nextCost = Math.ceil(game.quests[quest].baseCost * Math.pow(game.costMultiplier, game.quests[quest].owned));
           }
         }
       }
@@ -65,7 +72,9 @@ function goldClick() {
 }
 
 function autoGold() {
-  game.gold += game.gpsTotal * game.updateInterval/1000;
+  var timeElapsed = (Date.now() - game.lastUpdated) / 1000;
+  game.lastUpdated = Date.now();
+  game.gold += game.gpsTotal * timeElapsed;
   updateGold();
 }
 
@@ -94,15 +103,40 @@ function updateGps(){
 }
 
 function updateGold() {
-  goldString = game.gold.toFixed(0);
+  var goldString = '';
+  if(game.gold < 1000000) {
+    goldString = game.gold.toLocaleString('en-us', {maximumFractionDigits: 0});
+  }else{
+    goldString = game.gold.toExponential(2);
+  }
   document.getElementById("goldGained").innerHTML = goldString;
   gpsString = "Total gps: " + game.gpsTotal + " gold/s";
   document.getElementById("gpsTotalInfo").innerHTML = gpsString;
 }
 
 function updateButton(quest) {
-  var buttonId = quest + "Info";
-  document.getElementById(buttonId).innerHTML = "Owned: " + game.quests[quest].owned + ", cost: " + game.quests[quest].nextCost + " gold";
+  var buttonInfo = quest + "Info";
+  var costString = '';
+  var gpsString = '';
+  if(game.quests[quest].nextCost < 1000000) {
+    costString = game.quests[quest].nextCost.toLocaleString('en-us');
+  } else {
+    costString = game.quests[quest].nextCost.toExponential(2);
+  }
+  if(game.quests[quest].gps < 1000000){
+    gpsString = game.quests[quest].gps.toLocaleString('en-us');
+  }else{
+    gpsString = game.quests[quest].gps.toExponential(2);
+  }
+  document.getElementById(buttonInfo).innerHTML = "Owned: " + game.quests[quest].owned + ", cost: " + costString + " gold<br>" + gpsString + " gold/s";
+
+  if (game.gold < game.quests[quest].nextCost) {
+    document.getElementById(buttonInfo).parentElement.classList.add('btn-danger');
+    document.getElementById(buttonInfo).parentElement.classList.remove('btn-info');
+  }else{
+      document.getElementById(buttonInfo).parentElement.classList.remove('btn-danger');
+      document.getElementById(buttonInfo).parentElement.classList.add('btn-info');
+  }
 }
 
 function startQuest(quest) {
@@ -127,24 +161,48 @@ function resetGame() {
   updateGold();
 }
 
-// Initialize game
-window.addEventListener("load", function(){
-  game = initGameData();
-  for(quest in game.quests) {
-    updateButton(quest);
-  }
-  updateGold();
-});
-
 // Auto gain gold
 window.setTimeout(function(){
   window.setInterval(function(){
     autoGold();
   }, game.updateInterval);
-}, 2000)
+}, 100)
 
 
 // Save game every x milliseconds
 window.setInterval(function(){
   saveGame();
 }, 1000)
+
+// Initialize game
+window.addEventListener("load", function(){
+  game = initGameData();
+  updateGps();
+  for(quest in game.quests) {
+    updateButton(quest);
+  }
+
+  //Generate mana based on time offline
+  var savegame = JSON.parse(localStorage.getItem("dungeonClickerSave"))
+  if (savegame !== null) {
+    if(game.gpsTotal !== 0){
+      var timeDiff = Date.now() - game.lastSaveMS;
+      var timeDiffSecs = timeDiff / 1000;
+      var offlineProd = timeDiffSecs * game.gpsTotal * game.offlineMultiplier;
+      if (offlineProd > 1000000) {
+        var offProdString = offlineProd.toExponential(2);
+      } else {
+        var offProdString = offlineProd.toLocaleString('en-us', {maximumFractionDigits: 0});
+      }
+      // Add string to modal telling player how much mana has been gained during offline time
+      document.getElementById("offlineModalBody").innerHTML = "You have been offline for " + timeDiffSecs.toFixed(1) + " seconds."
+      document.getElementById("offlineModalBody").innerHTML += "<br>Your quests have yielded " + offProdString + " gold while offline!";
+      // Toggling modal
+      $("#offlineProdModal").modal();
+      //Updating mana
+      game.gold += offlineProd;
+    }
+  }
+
+  updateGold();
+});
